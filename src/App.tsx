7d10fa105f10c1b533f7fb4de42c848eef275251
YashwanthK <yashwanthkothakota@gmail.com>
2023-08-18 17:42:22 +0530

    using state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
+import Todo from "./models/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   return (
     <div className="App">
       <TodoList todos={} />
